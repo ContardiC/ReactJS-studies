2cb5c80a27f428af719b37dc7a1669a5bef4d3fc
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2023-06-20 10:26:48 +0200

    add Date and Counter

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,26 +1,71 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>Hello World!</div>
-      </header>
+      
+      <MyComponent/>
+      <Calendar/>
     </div>
   );
+}
+const CurrendDate = (props) => {
+  return(
+    <div>
+      <p>The current date is: {props.date}</p>
+    </div>
+  );
+}
+class Calendar extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div>
+        <h3>What date is it?</h3>
+        <CurrendDate date={Date()}/>
+        <Counter/>
+      </div>
+      
+      
+    
+    )
+  }
+}
+class MyComponent extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>Hello World!</div>
+    );
+  }
+}
+class Counter extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+    this.increment = this.increment.bind(this);
+  }
+  increment(){
+    this.setState(state =>({
+      count: state.count + 1
+    }));
+  }
+  render(){
+    return(
+      <div>
+        <button onClick={this.increment}>Increment!</button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
